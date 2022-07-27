62ee6ff338eea7d7060641be39e3139c5b9fdb16
Jakob Bouchard <jakob@jakobbouchard.dev>
2022-07-27 11:13:37 -0400

    Add favicons

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -7,6 +7,16 @@ export default defineConfig({
 	title: "Hestia Control Panel",
 	description: "Open-source web server control panel.",
 
+	head: [
+		["link", { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" }],
+		["link", { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" }],
+		["link", { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" }],
+		["link", { rel: "manifest", href: "/site.webmanifest" }],
+		["link", { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#b7236a" }],
+		["meta", { name: "msapplication-TileColor", content: "#b7236a" }],
+		["meta", { name: "theme-color", content: "#b7236a" }],
+	],
+
 	lastUpdated: true,
 
 	themeConfig: {

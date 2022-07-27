7a5d89d273ce1e260b636856c603c681feb0f317
Jakob Bouchard <jakob@jakobbouchard.dev>
2022-07-27 12:21:24 -0400

    Better color contrast in dark mode

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,4 +1,7 @@
 import DefaultTheme from "vitepress/theme";
-import "./custom.css";
+import "./base.css";
+import "./components/vp-doc.css";
+import "./components/custom-block.css";
+import "./vars.css";
 
 export default DefaultTheme;

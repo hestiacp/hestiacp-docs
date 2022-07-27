4b0dcb6ca68d536c02a0fc7035ca5be21f66fffd
Jakob Bouchard <jakob@jakobbouchard.dev>
2022-07-27 12:28:01 -0400

    Better way to control colors

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,7 +1,4 @@
 import DefaultTheme from "vitepress/theme";
-import "./base.css";
-import "./components/vp-doc.css";
-import "./components/custom-block.css";
 import "./vars.css";
 
 export default DefaultTheme;
